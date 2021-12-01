ef9cfe3444331b09773f94beee281d26fc6eb3dd
lzurbriggen <2656903+lzurbriggen@users.noreply.github.com>
2021-12-01 14:12:41 +0000

    Deploying to pages from @ jshmrtn/vue-haystack@962a6793ebfeee94ebbb51921a5caa686c71270d 🚀

---
+++ assets/generic.html.c0114956.js
@@ -0,0 +1,1 @@
+const e={key:"v-11610adb",path:"/generic.html",title:"Generic store",lang:"en-US",frontmatter:{},excerpt:"",headers:[],filePathRelative:"generic.md",git:{updatedTime:163836791e4,contributors:[{name:"Leo Zurbriggen",email:"zurbriggen@joshmartin.ch",commits:1}]}};export{e as data};
